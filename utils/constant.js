7743183618e2b579678831d122ccd93fdae2b45c
Muhammad Hafizh Abdillah <hafizh.abdillahh@gmail.com>
2020-01-27 01:01:23 +0700

    add table todo

--- utils/constant.js
+++ utils/constant.js
@@ -12,5 +12,22 @@ module.exports =  {
     CREATE INDEX IF NOT EXISTS username_user_idx ON usertodo (username, id); 
     
     ALTER TABLE usertodo ADD COLUMN IF NOT EXISTS name VARCHAR NOT NULL;
+
+    CREATE TABLE IF NOT EXISTS todo (
+        id BIGSERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        endDate TIMESTAMP NOT NULL,
+        c_at TIMESTAMP DEFAULT NOW(),
+        u_at TIMESTAMP DEFAULT NOW(),
+        deleted BOOLEAN DEFAULT FALSE,
+        isDone BOOLEAN DEFAULT FALSE,
+        userId INT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES usertodo (id)
+    );
+
+    CREATE INDEX IF NOT EXISTS id_user_todo_idx ON todo (id);
+    CREATE INDEX IF NOT EXISTS name_todo_idx ON todo (name, id);
+    CREATE INDEX IF NOT EXISTS date_todo_idx ON todo (c_at, id);
+    CREATE INDEX IF NOT EXISTS endDate_todo_idx ON todo (endDate, id);
     `
 }
